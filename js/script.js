2b22aaf934183b67bed63862bfbb64a4d1d11733
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-11-08 20:58:37 -0800

    Add JS to JS folder.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$("document").ready(function($){
+    var nav = $('.desktop-nav');
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 202) {
+          nav.addClass("fixed");
+      } else {
+          nav.removeClass("fixed");
+      }
+  });
+});
+
+$("document").ready(function($){
+  var nav = $('.handle');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 202) {
+      nav.addClass("fixed");
+    } else {
+      nav.removeClass("fixed");
+    }
+  });
+});
+
+$('.handle').on('click', function(){
+	$('nav ul').toggleClass('showing');
+});
