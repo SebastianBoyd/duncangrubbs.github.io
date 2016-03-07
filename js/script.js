6a9c100bad5abace800efe4a2de9a3b978a690bc
duncangrubbs <duncan.grubbs@gmail.com>
2016-03-07 12:05:48 -0800

    Misc. changes.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $("document").ready(function($){
     var nav = $('ul');
 
     $(window).scroll(function () {
-      if ($(this).scrollTop() >= 202) {
+      if ($(this).scrollTop() > 202) {
           nav.addClass("fixed");
           console.log("Ba");
       } else if ($(this).scrollTop() <= 202){
