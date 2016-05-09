079a8fb3c68ce43273d3f98ed41bc383aa102254
duncangrubbs <duncan.grubbs@gmail.com>
2016-05-09 12:30:20 -0700

    Become more offensive.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ var list = ['person', 'coder', 'singer', 'prostitute', 'hooker', 'astronaut', 'l
  'grave robber', 'american', 'asian', 'bride', 'son', 'father', 'divorcee', 'mother', 'man', 'woman', 'european', 'african', 'pornstar', 'crab fishermen', 'pilot', 'chef', 'policeman', 'airline chef', 'elevator music artist', 'child',
   'grandmother', 'grandfather', 'explosives tester', 'hacker', 'racer', 'l33t speak translator', 'gamer', 'feminist', 'gamer gril', 'mathmatics teacher', 'history major', 'vlogger', 'unemployed', 'professor', 'politician', 'racist', 'child soldier', 'pedophile', 'serial killer', 'nazi',
   'terrorist', 'best boy', 'dat boi', 'fuccboi', 'ghost producer', 'edm fan', 'piece of lint', 'communist', 'anarchist', 'prankster', 'gringo', 'social experimenter', 'scientist', 'mexican', 'slave', 'pimp', 'garbage man', 'cuuuuuck', 'cuckboi', 'ass eater',
-  'dweeb', 'nerd', 'russian', 'south african', 'child slave', 'ISIS', 'new yorker'];
+  'dweeb', 'nerd', 'russian', 'south african', 'child slave', 'ISIS', 'new yorker', 'ryan', 'duncan', 'clop sexual'];
 var $message = document.querySelector('#message');
 var intervalID = window.setInterval(myCallback, 200);
 
