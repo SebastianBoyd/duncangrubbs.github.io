eec87a712aec757ad239dc6546e2c9cda9c7db82
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-05-27 16:11:02 -0700

    Add two material standard boxes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
-var list = ['person', 'coder', 'singer', 'prostitute', 'hooker', 'astronaut', 'lameo', 'cat care giver', 'stripper', 'businessman', 'engineer', 'postman', 'artist', 'nurse', 'doctor', 'soldier',
- 'grave robber', 'american', 'asian', 'bride', 'son', 'father', 'divorcee', 'mother', 'man', 'woman', 'european', 'african', 'pornstar', 'crab fishermen', 'pilot', 'chef', 'policeman', 'airline chef', 'elevator music artist', 'child',
-  'grandmother', 'grandfather', 'explosives tester', 'hacker', 'racer', 'l33t speak translator', 'gamer', 'feminist', 'gamer gril', 'mathmatics teacher', 'history major', 'vlogger', 'unemployed', 'professor', 'politician', 'racist', 'child soldier', 'pedophile', 'serial killer', 'nazi',
-  'terrorist', 'best boy', 'dat boi', 'fuccboi', 'ghost producer', 'edm fan', 'piece of lint', 'communist', 'anarchist', 'prankster', 'gringo', 'social experimenter', 'scientist', 'mexican', 'slave', 'pimp', 'garbage man', 'cuuuuuck', 'cuckboi', 'ass eater',
-  'dweeb', 'nerd', 'russian', 'south african', 'child slave', 'ISIS', 'new yorker', 'ryan', 'duncan', 'clop sexual', 'vim user', 'cloptrop enthusiast', 'chalupa', 'nigerian prince', 'taliban', 'acid burn victim', 'cuckold', 'al nursra', 'al shabaab', 'muslim',
-  'christian', 'hindi', 'atheist', 'snowden', 'drag queen', 'dictator', 'dominatrix', 'cis scum', 'sjws'];
-var $message = document.querySelector('#message');
-var intervalID = window.setInterval(myCallback, 200);
+var $blockOne = document.querySelector('#block-one');
 
-function myCallback() {
-  var i = Math.floor((Math.random() * list.length));
-  $message.textContent = list[i] + '.';
+function addListeners () {
+  if(window.addEventListener) {
+    $blockOne.addEventListener('click', drop, false);
+  } else {
+    console.log('RIP');
+  }
+
+  function drop () {
+
+  }
 
 }
+
+window.onload = addListeners;
