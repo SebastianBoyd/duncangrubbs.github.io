34c91a01c7a1b2718b98ff9022b106d13611df00
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-06-23 16:59:04 -0700

    Remove element.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
-var $sideNav = document.querySelector('side-nav');
-
+var $sideNav = document.querySelector('#nav');
+var num = 0;
 function drawer() {
-  
+  if (num%2 === 0) {
+    nav.style.display = "flex";
+  }else{
+    nav.style.display = "none";
+  }
+  num++;
 }
